ac4cd779560074c326fcf376efbdaf39ab2334be
João Artur <61556834+JoaooArtur@users.noreply.github.com>
2024-12-05 10:02:11 -0300

    Update AutomaticGardener.ino

--- AutomaticGardener.ino
+++ AutomaticGardener.ino
@@ -1,23 +1,39 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
 #include <DHT.h>
 
-#define pinoDHT11 4
+#define pinoDHT11 D4
 #define DHTTYPE DHT11  
+
+const char* ssid = "Eduardo";
+const char* password = "12345678";
+
+const char* apiUrl = "https://6fwmz9m4gf.execute-api.us-east-1.amazonaws.com/default/automatic-gardener-api";
 
 DHT sensorDHT11(pinoDHT11, DHTTYPE);
 
-const int led = 8;
 const int sensor_pin = A0;
 int sensor;
 const int threshold = 50;
 
 void setup() {
-  pinMode(led, OUTPUT);
-  sensorDHT11.begin();
   Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando-se ao Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConectado ao Wi-Fi!");
+  sensorDHT11.begin();
 }
 
 void loop() {
-  sensor = analogRead(sensor_pin);
+  int sunlight = analogRead(sensor_pin);
 
   float humidity = sensorDHT11.readHumidity();
   float temperature = sensorDHT11.readTemperature();
@@ -26,18 +42,43 @@ void loop() {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
+
+  int humidityInt = (int)humidity;
+  int temperatureInt = (int)temperature;
+
   Serial.print("Humidity: ");
-  Serial.print(humidity);
+  Serial.print(humidityInt);
   Serial.print(" %\t");
   Serial.print("Temperature: ");
-  Serial.print(temperature);
+  Serial.print(temperatureInt);
   Serial.println(" *C");
+
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClientSecure client;
+    client.setInsecure();
+
+    HTTPClient http;
+
+    http.begin(client, apiUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    String jsonBody = String("{\"key1\": ") + humidityInt +
+                      String(", \"key2\": ") + temperatureInt +
+                      String(", \"key3\": ") + sunlight + String("}");
+
+    int httpResponseCode = http.POST(jsonBody);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("Resposta da API: " + response);
+    } else {
+      Serial.println("Erro na requisição: " + String(httpResponseCode));
+    }
+
+    http.end();
+  } else {
+    Serial.println("Erro ao conectar ao Wi-Fi.");
+  }
   
-  // Serial.println(sensor);
-  if(sensor<threshold){
-    digitalWrite(led, HIGH);
-  }
-  else{
-    digitalWrite(led, LOW);
-  }
+  delay(300000);
 }
